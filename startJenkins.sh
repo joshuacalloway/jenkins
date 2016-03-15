7e2df479cfe58f7086f233dd2174d6e0a25b19fe
Joshua Calloway <joshua.calloway@gmail.com>
2016-03-15 00:15:25 -0500

    added start script

--- startJenkins.sh
+++ startJenkins.sh
@@ -1,4 +1,3 @@
 
-docker run -v /var/run/docker.sock:/var/run/docker.sock \
-           -v $(which docker):/bin/docker \
-           -ti -p 8080:8080 jenkins
+docker run --privileged -d -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker -v /lib64/libsystemd-journal.so.0:/usr/lib/libsystemd-journal.so.0 -v /lib64/libsystemd-id128.so.0:/usr/lib/libsystemd-id128.so.0 -v /lib64/libdevmapper.so.1.02:/usr/lib/libdevmapper.so.1.02 -v /lib64/libgcrypt.so.11:/usr/lib/libgcrypt.so.11 -v /lib64/libdw.so.1:/usr/lib/libdw.so.1 -v /lib64/libudev.so.0:/usr/lib/libudev.so.0 -p 8080:8080 jenkins
+
